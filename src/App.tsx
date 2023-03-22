bb265d6825e368d9bde222acd3e8fa235809625c
Walber Cesar de sousa santos <walber.santos01@etec.sp.gov.br>
2023-03-22 16:46:55 -0300

    create theme and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/themes/global";
 export function App() {
-  return <></>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hellow world</h1>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
