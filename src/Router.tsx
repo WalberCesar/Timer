eb7cc76cd61d18d8a4806d49b166091000d7ecc9
Walber Cesar de sousa santos <walber.santos01@etec.sp.gov.br>
2023-04-01 12:39:55 -0300

    style Header

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
-import { Routes, Route } from "react-router-dom";
-import { DefaultLayout } from "./layouts/DefaultLayout";
-import { History } from "./pages/history";
-import { Home } from "./pages/home";
+import { Routes, Route } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout'
+import { History } from './pages/history'
+import { Home } from './pages/home'
 
 export function Router() {
   return (
@@ -11,5 +11,5 @@ export function Router() {
         <Route path="/history" element={<History />} />
       </Route>
     </Routes>
-  );
+  )
 }
